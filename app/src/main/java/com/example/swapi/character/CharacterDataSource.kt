9ebe7c9e4a19b0c4d01185f6498c951c22734548
Andrej Kuznecov <aku@tiatechnology.com>
2020-08-26 17:11:05 +0300

    New classes

--- app/src/main/java/com/example/swapi/character/CharacterDataSource.kt
+++ app/src/main/java/com/example/swapi/character/CharacterDataSource.kt
@@ -1,22 +1,23 @@
 package com.example.swapi.character
 
 import android.util.Log
+import androidx.paging.DataSource
 import androidx.paging.PageKeyedDataSource
 import com.example.swapi.network.Character
 import com.example.swapi.network.Swapi
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.cancel
-import kotlinx.coroutines.launch
+import com.example.swapi.usecase.GetPeopleUseCase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 
 private const val FIRST_PAGE = 1
 
-class CharacterDataSource(private val scope: CoroutineScope) :
+class CharacterDataSource(private val getPeopleUseCase: GetPeopleUseCase = GetPeopleUseCase.create()) :
     PageKeyedDataSource<Int, Character>() {
     override fun loadInitial(
         params: LoadInitialParams<Int>,
         callback: LoadInitialCallback<Int, Character>
     ) {
-        scope.launch {
+        GlobalScope.async {
             try {
                 val response = Swapi.retrofitService.getPeopleAsync(FIRST_PAGE)
                 when {
@@ -24,27 +25,33 @@ class CharacterDataSource(private val scope: CoroutineScope) :
                         val characterList = response.await().results
                         callback.onResult(characterList, null, FIRST_PAGE.inc())
                     }
+                    else -> Log.e("CharacterDataSource", "loadInitial else has failed")
                 }
             } catch (e: Exception) {
                 Log.e("CharacterDataSource", "loadInitial has failed")
             }
-//            var getPeopleDeferred = Swapi.retrofitService.getPeopleAsync(1)
-//            try {
-//                //status = loading
-//                _swapiResult.value = getPeopleDeferred.await()
-//                _characterList.value = _swapiResult.value!!.results
-//            } catch (e: Exception) {
-//
-//            }
         }
     }
 
     override fun loadBefore(params: LoadParams<Int>, callback: LoadCallback<Int, Character>) {
-        TODO("Not yet implemented")
+        GlobalScope.async {
+            try {
+                val response = Swapi.retrofitService.getPeopleAsync(params.key)
+                when {
+                    response.isCompleted -> {
+                        val characterList = response.await().results
+                        callback.onResult(characterList, params.key.dec())
+                    }
+                    else -> Log.e("CharacterDataSource", "loadBefore else has failed")
+                }
+            } catch (e: Exception) {
+                Log.e("CharacterDataSource", "loadBefore has failed")
+            }
+        }
     }
 
     override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, Character>) {
-        scope.launch {
+        GlobalScope.async {
             try {
                 val response = Swapi.retrofitService.getPeopleAsync(params.key)
                 when {
@@ -52,6 +59,7 @@ class CharacterDataSource(private val scope: CoroutineScope) :
                         val characterList = response.await().results
                         callback.onResult(characterList, params.key.inc())
                     }
+                    else -> Log.e("CharacterDataSource", "loadAfter else has failed")
                 }
             } catch (e: Exception) {
                 Log.e("CharacterDataSource", "loadAfter has failed")
@@ -59,8 +67,10 @@ class CharacterDataSource(private val scope: CoroutineScope) :
         }
     }
 
-    override fun invalidate() {
-        super.invalidate()
-        scope.cancel()
+    companion object {
+
+        fun factory() = object : DataSource.Factory<Int, Character?>() {
+            override fun create() = CharacterDataSource()
+        }
     }
 }
---
+++ app/src/main/java/com/example/swapi/character/CharacterPagedListProvider.kt
@@ -0,0 +1,35 @@
+package com.example.swapi.character
+
+import androidx.lifecycle.LiveData
+import androidx.paging.DataSource
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.example.swapi.network.Character
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class CharacterPagedListProvider(private val factory: DataSource.Factory<Int, Character?>) :
+    PagedListProvider<Character?> {
+
+    override fun provide(): LiveData<PagedList<Character?>> {
+
+        return LivePagedListBuilder(
+            factory, PagedList.Config.Builder()
+                .setEnablePlaceholders(false)
+                .setPageSize(20)
+                .setInitialLoadSizeHint(20)
+                .build()
+        ).setFetchExecutor(createFetchExecutor())
+            .build()
+    }
+
+    private fun createFetchExecutor(): ExecutorService {
+        val nThreads = Runtime.getRuntime().availableProcessors() + 1
+        return Executors.newFixedThreadPool(nThreads)
+    }
+}
+
+interface PagedListProvider<Value> {
+
+    fun provide(): LiveData<PagedList<Value>>
+}
