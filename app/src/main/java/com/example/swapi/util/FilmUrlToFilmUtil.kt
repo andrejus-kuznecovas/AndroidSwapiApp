dcc15b556ff5c8e110a9a6f818198fa7ab61ebb9
Andrej Kuznecov <aku@tiatechnology.com>
2020-10-15 11:55:15 +0300

    Add todo plan

--- app/src/main/java/com/example/swapi/util/FilmUrlToFilmUtil.kt
+++ app/src/main/java/com/example/swapi/util/FilmUrlToFilmUtil.kt
@@ -2,6 +2,19 @@ package com.example.swapi.util
 
 import com.example.swapi.model.Film
 
+/**
+ * 1. Add repo and call api from repo only
+ * 2. Add dagger
+ * 3. Clean the arch
+ * 4. Intro page with animation
+ * 5. UI cleanup, use Material D
+ * 6. Testing testing
+ * 7. gradle cleanup and version updates
+ *
+ * BUGS:
+ * 1. app crash on rotation
+ * 2. search filtering
+ * */
 class FilmUrlToFilmUtil {
     companion object {
         fun filmUrlListToFilmList(filmUrls: List<String>, allMovies: List<Film>): List<Film> {
